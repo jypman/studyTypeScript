5b9a2bdcf6f78074915c2fa1dd870517bd6c30bc
JINYOUNG PARK <asd029600@gmail.com>
2021-09-02 03:00:50 +0900

    02-3 공부

--- dist/utils/intro.js
+++
@@ -1,7 +0,0 @@
-//현재 경로에서 터미널 명령어 tsc {특정 typescript파일이름}을 입력하면 해당 typescript파일이 js파일로 컴파일된다.
-// typescript파일을 변경하며 실시간으로 컴파일 되는 것을 원한다면 tsc -w {특정 typescript파일이름}로 입력하면 된다.
-var logName = function (name) {
-    console.log(name);
-};
-logName('jyp');
-//# sourceMappingURL=intro.js.map
