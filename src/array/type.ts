5219651b9a221d4db07f6d0a22d6c9729802c083
JINYOUNG PARK <asd029600@gmail.com>
2021-10-03 22:50:19 +0900

    배열 타입 공부

---
+++ src/array/type.ts
@@ -0,0 +1,7 @@
+const numArray : number[] = [1,2,3]
+const strArray : string[] = ['a','b','c']
+
+type user = {name : string, age? : number}
+const userArray : user[] = [{name : "jyp"}, {name : "fgh", age : 17}]
+
+console.log('numArray :', numArray, 'strArray :', strArray, 'userArray :', userArray)
